3f013416ceedd88c2a3069086f194f0afb6008c1
xioinghui <melot@xionghui>
2021-09-22 16:39:34 +0800

    feat:测试

--- src/components/Crud/crud.js
+++ src/components/Crud/crud.js
@@ -1,4 +1,5 @@
-// import Vue from 'vue'
+import Vue from 'vue'
+import { initData } from '@/api/data'
 
 function CRUD(options) {
   /**
@@ -23,14 +24,87 @@ function CRUD(options) {
     sort: ['id,desc']
   }
   options = mergeOptions(defaultOptions, options)
+  const data = {
+    ...options,
+    page: {
+      page: 1,
+      size: 10,
+      total: 0
+    }
+  }
+  const crud = Object.assign({}, data)
+  const methods = {
+    toQuery(pageDefault = true) {
+      if (pageDefault) {
+        crud.page.page = 1
+      }
+      crud.refresh()
+    },
+    refresh() {
+      if (!callVmhook(crud, CRUD.callVmhook.beforeRefresh)) {
+        return
+      }
+      return new Promise((resolve, reject) => {
+        crud.loading = true
+        const paramsData = crud.getQueryParams()
+        initData(crud.url, paramsData).then(data => {
+          const table = crud.getTable()
+          if (table && table.lazy) {
+            table.store.states.treeData = {}
+            table.store.states.lazyTreeNodeMap = {}
+          }
+          crud.page.total = data.data.count
+          crud.data = data.data
+          // crud.resetDataStatus()
+        })
+      })
+    },
+    getQueryParams() {
+      Object.key(crud.query).length !== 0 && Object.key(crud.query).forEach(item => {
+        if (crud.query[item] === null || crud.query[item] === '') crud.query[item] = undefined
+      })
+      return {
+        pageIndex: crud.page.page,
+        pageCount: crud.page.size,
+        sort: crud.sort,
+        ...crud.query
+      }
+    },
+    getTable() {
+      return this.findVM('presenter').$refs.table
+    },
+    // resetDataStatus(){
+    //   const dataStatus={}
+    //   function resetStatus(datas){
+    //     datas&&datas.forEach(e=>{
+    //       dataStatus[crud.getDataId(e)]={
+    //         delete:0,
+    //         edit:0
+    //       }
+    //       if(e.children){
+    //         resetStatus(e.children)
+    //       }
+    //     })
+    //   }
+    //   resetStatus(crud.data[crud.listName])
+    //   crud.dataStatus=dataStatus
+    // },
+    getDataId(data) {
+      return data[this.idField] || data.aid
+    }
+  }
+  Vue.observable(crud)
+  Object.assign(crud, methods)
 }
 
 function mergeOptions(src, opts) {
-  // const optsRet = {
-  //   ...src
-  // }
-  // for(const key in src){
-  //   if(optsRet)
-  // }
+  const optsRet = {
+    ...src
+  }
+  for (const key in src) {
+    if (Object.prototype.hasOwnProperty.call(opts, key)) {
+      optsRet[key] = opts[key]
+    }
+  }
+  return optsRet
 }
-CRUD()
